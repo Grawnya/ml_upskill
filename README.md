57b5fb9bafee4baeb2131410d5c96e387d495fde
Grawnya <grainnejdonegan@gmail.com>
2023-12-07 00:23:19 +0100

    preprocess images for splitting into test and train

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ml_upskill
 
-Sources: 
+## Sources: 
 * To get images: [A simple way to collect your deep learning image dataset](https://medium.com/analytics-vidhya/a-simple-way-to-collect-your-deep-learning-image-dataset-4ead47b6826c)
+* Image size influence: [How to Pick the Optimal Image Size for Training Convolution Neural Network?](https://medium.com/analytics-vidhya/how-to-pick-the-optimal-image-size-for-training-convolution-neural-network-65702b880f05)
+* Imread OpenCV Functionality: [OpenCV Docs](https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56)
+* RGB Conversion: [Matplotlib requires RGB](https://stackoverflow.com/questions/54959387/rgb-image-display-in-matplotlib-plt-imshow-returns-a-blue-image)
 
+## Development Bugs:
+* Error when trying to resize, but one image was of type `.gif`.
--- get-images.py
+++ get-images.py
@@ -1,16 +1,19 @@
 from bing_image_downloader import downloader
+import os
+import cv2
+import numpy as np
 
+# Dealing with obtaining images
 def download_images(request):
 
-    """Store downloaded images in a directory.
+    """
+    Store downloaded images in a directory.
 
-        Parameters:
-
+    Parameters:
     request: list of queries to search for
 
-        # Return: Images downloaded that match request
-
-        """
+    Returns: Images downloaded that match request
+    """
 
     downloader.download(request, 
                         limit=20, 
@@ -18,7 +21,45 @@ def download_images(request):
                         force_replace=False)
     
 
-requests = ['laptop computer', 'computer monitor', 'computer mouse', 'soccer ball', 'bee', 'locomotive train', 'letter m', 'letter t']
+requests = ['laptop computer',
+            'computer monitor',
+            'computer mouse',
+            'soccer ball',
+            'bee',
+            'locomotive train',
+            'letter m',
+            'letter t']
 
-for request in requests:
-    download_images(request)
+# comment out below once completed if process is to be repeated
+# for request in requests:
+#     download_images(request)
+
+# based on neural network example, the images should be the same size
+def convert_image_and_class_set_to_np_arrays(folder):
+    images = []
+    classes = []
+
+    # can take advantage of sorting from previous step to get names of classes
+    for class_name in os.listdir(folder):
+        class_folder = os.path.join(folder, class_name) # automatically adds backslash for windows
+
+        # when in the class, go through the images and clean them for the ML
+        for file_name in os.listdir(class_folder):
+            image_path = os.path.join(class_folder, file_name)
+
+            # to load the image an preprocess it
+            image = cv2.imread(image_path)
+            image = cv2.resize(image, (300, 300))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Matplotlib requires RGB
+            images.append(image)
+            classes.append(class_name)
+
+    image_array_creation = np.array(images)
+    image_classes_creation = np.array(classes)
+
+    return image_array_creation, image_classes_creation
+
+image_array, image_classes = convert_image_and_class_set_to_np_arrays('images')
+
+print(image_array[0])
+print(image_classes[0])
